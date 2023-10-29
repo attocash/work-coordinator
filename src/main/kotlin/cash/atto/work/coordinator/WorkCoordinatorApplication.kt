4402795f15e54df1ead7f9eda9a0ce121365e3e8
Felipe <felipe@rotilho.com>
2023-10-29 16:22:45 +0100

    Initial commit

---
+++ src/main/kotlin/cash/atto/work/coordinator/WorkCoordinatorApplication.kt
@@ -0,0 +1,11 @@
+package cash.atto.work.coordinator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkCoordinatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorkCoordinatorApplication>(*args)
+}
---
+++ src/test/kotlin/cash/atto/work/coordinator/WorkCoordinatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package cash.atto.work.coordinator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkCoordinatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
