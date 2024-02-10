6c4e3896751e5d81452d0d5cb5cefc07a5bf6c0c
Felipe <felipe@rotilho.com>
2024-02-10 15:11:26 +0100

    Attempt graalvm

--- src/main/kotlin/cash/atto/work/Application.kt
+++ src/main/kotlin/cash/atto/work/Application.kt
@@ -4,11 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            runApplication<Application>(*args)
-        }
-    }
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
+
